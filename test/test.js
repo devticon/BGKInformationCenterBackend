04fabd16c8921b70f20059bf60bf2496b659ef94
krs <k.kamasa@gmail.com>
2020-11-29 09:14:52 +0100

    rss

--- test/test.js
+++ test/test.js
@@ -1,8 +1,4 @@
 const Gun = require("gun");
-const gun = Gun("https://peaceful-woodland-02086.herokuapp.com//gun");
+const gun = Gun("https://peaceful-woodland-02086.herokuapp.com/gun");
 
-gun
-  .get(
-    "3cda5bc2-6ddd-4e74-8c55-dea0cd292593/teams/0106420f-7661-4ce8-9302-6edf33c96c0f/channels/19:be68d750843f40aca9c7c6f1fd349598@thread.tacv2/messages"
-  )
-  .on((a) => console.log(a));
+gun.get("3cda5bc2-6ddd-4e74-8c55-dea0cd292593/teams").on((a) => console.log(a));
