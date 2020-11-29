b6aee0c97c15ec69539734f3b08d6945001e13f2
krs <k.kamasa@gmail.com>
2020-11-29 08:45:14 +0100

    rss

--- test/test.js
+++ test/test.js
@@ -1,4 +1,8 @@
 const Gun = require("gun");
 const gun = Gun("https://devticon.loca.lt/gun");
 
-gun.get("1fa117f9-b061-4c10-9309-2f15a875a9c0/teams").on((a) => console.log(a));
+gun
+  .get(
+    "3cda5bc2-6ddd-4e74-8c55-dea0cd292593/teams/0106420f-7661-4ce8-9302-6edf33c96c0f/channels/19:2f3eb6a112a749bcb06f2af697cf35bd@thread.tacv2"
+  )
+  .on((a) => console.log(a));
