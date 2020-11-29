725189a3bdc36e6f516372611617c9426d63ca5e
krs <k.kamasa@gmail.com>
2020-11-29 08:48:41 +0100

    rss

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
 const Gun = require("gun");
-const gun = Gun("https://devticon.loca.lt/gun");
+const gun = Gun("https://peaceful-woodland-02086.herokuapp.com//gun");
 
 gun
   .get(
-    "3cda5bc2-6ddd-4e74-8c55-dea0cd292593/teams/0106420f-7661-4ce8-9302-6edf33c96c0f/channels/19:2f3eb6a112a749bcb06f2af697cf35bd@thread.tacv2"
+    "3cda5bc2-6ddd-4e74-8c55-dea0cd292593/teams/0106420f-7661-4ce8-9302-6edf33c96c0f/channels/19:be68d750843f40aca9c7c6f1fd349598@thread.tacv2/messages"
   )
   .on((a) => console.log(a));
